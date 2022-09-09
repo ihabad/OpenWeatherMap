3a2ec9d092ac995ee9966f0ea61d7dcbb87ac52b
Ihab Abou Diab <106569891+ihabad@users.noreply.github.com>
2022-09-09 15:40:37 -0700

    Add files via upload

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMap
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsetting.json");
+            string ApiKey = JObject.Parse(key).GetValue("ApiKey").ToString();
+
+            Console.WriteLine("Check by ZipCode");
+            var ZipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={{zip code}},{{country code}}&appid={{API key}}";
+
+            Console.WriteLine($"As of now, the current temperature is {WeatherMap.GetTemp(apiCall)} degrees fahrenheit ");
+        }
+    }
+}
---
+++ OpenWeatherMap/WeatherMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace OpenWeatherMap
+{
+    public class WeatherMap
+    {
+
+        public static double GetTemp(string apiCall)
+        {
+
+          var client = new HttpClient();
+
+          var response = client.GetAsync(apiCall).Result;
+          var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+          return temp;
+         
+
+        }
+    }
+}
